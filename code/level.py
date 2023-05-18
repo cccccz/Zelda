34810cb9eac5121739675bde19f52efb90434f8f
cccccz <chenzheng0430@gmail.com>
2023-05-18 11:56:13 +0800

    organized, handled basicmovement and collision

---
+++ code/level.py
@@ -0,0 +1,36 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
+
+class Level:
+    def __init__(self):
+
+        #get the display surface
+        self.dispaly_surface = pygame.display.get_surface()
+        
+        #sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+        
+        #sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index*TILESIZE
+                y = row_index*TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+                if col =='p':
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
+
+    def run(self):
+        self.visible_sprites.draw(self.dispaly_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    pass

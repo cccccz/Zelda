2926db51669443eb3ef43004f04d9423e74d1e58
cccccz <chenzheng0430@gmail.com>
2023-05-24 01:06:12 +0800

    fixed a typo in settings, now have weapon ui

--- code/settings.py
+++ code/settings.py
@@ -25,9 +25,9 @@ UI_BORDER_COLOR_ACTIVE = 'gold'
 
 # weapons
 weapon_data = {
-    'sword':{'cooldown':100,'damage':15,'graphic':'../gtaphics/weapons/sword/full.png'},
-    'lance':{'cooldown':400,'damage':30,'graphic':'../gtaphics/weapons/lance/full.png'},
-    'axe':{'cooldown':300,'damage':20,'graphic':'../gtaphics/weapons/axe/full.png'},
-    'rapier':{'cooldown':50,'damage':8,'graphic':'../gtaphics/weapons/rapier/full.png'},
-    'sai':{'cooldown':80,'damage':10,'graphic':'../gtaphics/weapons/sai/full.png'}
+    'sword':{'cooldown':100,'damage':15,'graphic':'../graphics/weapons/sword/full.png'},
+    'lance':{'cooldown':400,'damage':30,'graphic':'../graphics/weapons/lance/full.png'},
+    'axe':{'cooldown':300,'damage':20,'graphic':'../graphics/weapons/axe/full.png'},
+    'rapier':{'cooldown':50,'damage':8,'graphic':'../graphics/weapons/rapier/full.png'},
+    'sai':{'cooldown':80,'damage':10,'graphic':'../graphics/weapons/sai/full.png'}
 }

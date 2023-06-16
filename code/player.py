ea1728fb2b8778ac19a74847fb8c2e7b164a6fd2
cccccz <chenzheng0430@gmail.com>
2023-06-16 11:37:48 +0800

    Added Enemy animation, with proper attack cooldown

--- code/player.py
+++ code/player.py
@@ -1,9 +1,10 @@
 import pygame
 from settings import *
 from support import *
+from entity import Entity
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack):
+class Player(Entity):
+    def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack,create_magic):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
@@ -11,8 +12,6 @@ class Player(pygame.sprite.Sprite):
         # graphics
         self.import_player_assets()
         self.status = 'down'
-        self.frame_index = 0
-        self.animation_speed = 0.15
         # movements
         self.direction = pygame.math.Vector2()
         self.attacking = False
@@ -30,6 +29,16 @@ class Player(pygame.sprite.Sprite):
         self.weapon_switch_time = None
         self.switch_duration_cooldown = 200
         # print(self.weapon)
+
+        self.magic_index = 0
+        self.magic = list(magic_data.keys())[self.magic_index]
+        self.can_switch_magic = True
+        self.magic_switch_time = None
+        self.create_magic = create_magic
+
+
+
+
 
         # stats
         self.stats = {'health':100,'energy':60,'attack':10,'magic':4,'speed':6}
@@ -90,7 +99,11 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_LCTRL] and not self.attacking:
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
-                print('magic2')
+                self.magic = list(magic_data.keys())[self.magic_index]
+                style = self.magic
+                strength = magic_data[style]['strength'] + self.stats['magic']
+                cost = magic_data[style]['cost']
+                self.create_magic(style,strength,cost)
 
             if keys[pygame.K_q] and self.can_switch_weapon:
                 self.can_switch_weapon = False
@@ -102,6 +115,16 @@ class Player(pygame.sprite.Sprite):
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
                 print(self.weapon)
 
+            if keys[pygame.K_e] and self.can_switch_magic:
+                self.can_switch_magic = False
+                self.magic_switch_time = pygame.time.get_ticks()
+
+                if self.magic_index < len(list(magic_data.keys())) - 1:
+                    self.magic_index += 1
+                else:
+                    self.magic_index = 0
+
+                self.magic = list(magic_data.keys())[self.magic_index]
 
     def get_status(self):
         if self.direction.x == 0 and self.direction.y == 0:
@@ -120,39 +143,6 @@ class Player(pygame.sprite.Sprite):
         else:
             self.status = self.status.replace('_attack','')
 
-    def move(self,speed):
-        #without this the speed of moving diagonal will be faster 
-        #trignometry here!
-        if self.direction.magnitude()!=0:
-            self.direction = self.direction.normalize()
-
-        #self.rect.center += self.direction * speed
-        self.hitbox.x += self.direction.x * speed
-        self.collision("horizontal")
-        self.hitbox.y += self.direction.y * speed
-        self.collision("verticle")
-        self.rect.center = self.hitbox.center
-
-    def collision(self,direction):
-        if direction == "horizontal":
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0: #moving right
-                        self.hitbox.right = sprite.hitbox.left
-
-                    if self.direction.x < 0: #moving left
-                        self.hitbox.left = sprite.hitbox.right
-
-        
-        if direction == "verticle":
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0: #moving down
-                        self.hitbox.bottom = sprite.hitbox.top
-
-                    if self.direction.y < 0: #moving up
-                        self.hitbox.top = sprite.hitbox.bottom
-
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
 
@@ -164,7 +154,10 @@ class Player(pygame.sprite.Sprite):
         if not self.can_switch_weapon:
             if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
                 self.can_switch_weapon = True
-                
+
+        if not self.can_switch_magic:
+            if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_magic = True        
 
     def animate(self):
         animation = self.animations[self.status]
--- code/ui.py
+++ code/ui.py
@@ -18,6 +18,12 @@ class UI:
             path = weapon['graphic']
             weapon = pygame.image.load(path).convert_alpha()
             self.weapon_graphics.append(weapon)
+        
+        self.magic_graphics = []
+        for magic in magic_data.values():
+            path = magic['graphic']
+            magic= pygame.image.load(path).convert_alpha()
+            self.magic_graphics.append(magic)
 
     def show_bar(self,current,max_amount,bg_rect,color):
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -43,22 +49,31 @@ class UI:
 
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,text_rect.inflate(20,20),3)
 
-    def selection_box(self,left,top):
+    def selection_box(self,left,top, has_switched):
         bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
-        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+        if not has_switched:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR_ACTIVE,bg_rect,3)
+
+        else:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
         return bg_rect
         
-    def weapon_overlay(self,weapon_index):
-        bg_rect = self.selection_box(10,630)
+    def weapon_overlay(self,weapon_index,has_switched):
+        bg_rect = self.selection_box(10,630,has_switched)
         weapon_surf = self.weapon_graphics[weapon_index]
         weapon_rect =  weapon_surf.get_rect(center = bg_rect.center)
         self.display_surface.blit(weapon_surf,weapon_rect)
-        
+    
+    def magic_overlay(self,magic_index,has_switched):
+        bg_rect = self.selection_box(80,635,has_switched)
+        magic_surf = self.magic_graphics[magic_index]
+        magic_rect =  magic_surf.get_rect(center = bg_rect.center)
+        self.display_surface.blit(magic_surf,magic_rect)
 
     def display(self,player):
         self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
         self.show_bar(player.health,player.stats['health'],self.enerygy_bar_rect,ENERGY_COLOR)
         self.show_exp(player.exp)
-        self.weapon_overlay(player.weapon_index)
-        self.selection_box(80,635)
+        self.weapon_overlay(player.weapon_index,player.can_switch_weapon)
+        self.magic_overlay(player.magic_index,player.can_switch_magic)

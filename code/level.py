430a3e59caf2c43f088e931e56586e5d47781f58
cccccz <chenzheng0430@gmail.com>
2023-06-23 20:47:17 +0800

    adding particle effects(grass for now)

--- code/level.py
+++ code/level.py
@@ -8,6 +8,7 @@ from random import choice
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
+from particles import AnimationPlayer
 
 class Level:
     def __init__(self):
@@ -27,7 +28,11 @@ class Level:
         # sprite setup
         self.create_map()
 
+        # ui
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer()
 
     def create_map(self):
         layout = {
@@ -98,6 +103,8 @@ class Level:
                 if collision_sprites:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
+                            pos = target_sprite.rect.center
+                            self.animation_player.create_grass_particles(pos,self.visible_sprites)
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player,attack_sprite.sprite_type)
